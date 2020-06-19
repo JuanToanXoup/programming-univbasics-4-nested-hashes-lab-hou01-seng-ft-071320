879118540f1904d771130f1b57b24ee52c598d87
John Doan <johndoan42@gmail.com>
2020-06-19 17:52:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 def hopper
 	programmer_hash =
  		{
@@ -81,7 +81,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto] = [:known_for],
+programmer_hash[:yukihiro_matsumoto] = [:known_for]
+binding.pry
+puts "hello"
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby",
 programmer_hash[:yukihiro_matsumoto] = [:languages],
 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"],
